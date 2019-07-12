ecf8d6a7462d849e8dc33eeba4f4fb20870c24cb
Cody Beyer <cobey@microsoft.com>
2019-07-12 12:26:36 -0700

    Added Files

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=vslive-demo-cobey-Web-Deploy.pubxml -p:Password=azAllry1JXTy7yaFMKTbo8Bq7edltlrTtafPo4PxiGgujP44wnjnZZFhz7ie
+
